bd611c6f9acf1fe1383bae8fea93932086533c8d
Bacckkern1 <egorpetrovdz@gmail.com>
2023-03-01 22:45:41 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/timetable/MainActivity.kt
+++ app/src/main/java/com/example/timetable/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import com.example.timetable.databinding.ActivityMainBinding
-
+//комент
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
