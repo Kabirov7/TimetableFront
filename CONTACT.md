2582467980709eff6c092335e411ae1f6cd89808
IlyaBobkin <BobkaG@users.noreply.github.com>
2023-03-09 16:49:02 +0300

    newproj
    newproj

--- CONTACT.md
+++
@@ -1,4 +0,0 @@
-## CONTACT
-* you can contact me using this email misosvec01@gmail.com
-
-
---
+++ app/src/main/java/com/example/raspisanie/Week.kt
@@ -0,0 +1,24 @@
+package com.example.raspisanie
+
+import java.time.DayOfWeek
+import java.time.Month
+
+data class Week(
+    var list: List<Day>
+)
+
+data class Day(
+    var listOfLessons: List<Lesson>,
+    var date: String,
+    var dayOfWeek: String,
+    var month: String
+)
+
+
+data class Lesson
+    (
+    var time: String,
+    var teacher: String,
+    var discipline: String
+)
+
